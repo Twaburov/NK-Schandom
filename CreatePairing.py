44c345842b6b21e91fe34b9db005a20e986b4745
AzureAD\RobinSwinkels <robin@pipple.nl>
2024-04-23 15:38:19 +0200

    feat: standard formatting

--- CreatePairing.py
+++ CreatePairing.py
@@ -5,14 +5,17 @@ import random
 cup = []
 players = Player.players
 
+
 def MakePairing():
     CreatePairingNumbers()
     AssignPairingNumbers()
     CreatePairingDict()
 
+
 def CreatePairingNumbers():
     for i in range(len(players)):
-        cup.append(i+1)
+        cup.append(i + 1)
+
 
 def AssignPairingNumbers():
     logging.info("Assigning Pairing Numbers")
@@ -24,8 +27,10 @@ def AssignPairingNumbers():
         logging.info("Assigning pairing number " + str(player.pairingnumber) + " to player " + player.name)
         cup.remove(pairing_number)
 
+
+
+
 def CreatePairingDict():
     for p in players:
-        Player.pairingnr_to_name[p.pairingnumber]= p.name
+        Player.pairingnr_to_name[p.pairingnumber] = p.name
     Player.pairingnr_to_name = dict(sorted(Player.pairingnr_to_name.items()))
-
--- Import.py
+++ Import.py
@@ -1,37 +1,43 @@
 from Objects import *
+
 nr_of_players = 0
 
+
 def ImportAll(filename):
-    import openpyxl 
+    import openpyxl
     wb = openpyxl.load_workbook(filename)
 
     ImportPlayers(wb['Players'])
     ImportGames(wb['Games'])
     ImportPrefences(wb['Prefs'])
-    #Import the Berger tabel to initiate a Round Robin pairing.
+    # Import the Berger tabel to initiate a Round Robin pairing.
     ImportBerger(wb["Berger" + str(len(Player.players))])
-    
+
+
 def ImportPlayers(ws_players):
-    for i in range(2,ws_players.max_row+1):
-        Player(ws_players.cell(row=i,column=1).value, ws_players.cell(row=i,column=2).value)
+    for i in range(2, ws_players.max_row + 1):
+        Player(ws_players.cell(row=i, column=1).value, ws_players.cell(row=i, column=2).value)
     global nr_of_players
     nr_of_players = len(Player.players)
 
-def ImportGames(ws_games):
-    for i in range(2,ws_games.max_row+1):
-        Game(ws_games.cell(row=i,column=1).value)
 
-#Imports the Prefs table
+def ImportGames(ws_games):
+    for i in range(2, ws_games.max_row + 1):
+        Game(ws_games.cell(row=i, column=1).value)
+
+
+# Imports the Prefs table
 def ImportPrefences(ws_preferences):
-    for i in range(2,ws_preferences.max_column+1):
-        for j in range(2,ws_preferences.max_row+1):
-            Preference(ws_preferences.cell(row=j,column=i).value,
-                GetPlayerByName(ws_preferences.cell(row=1,column=i).value),
-                GetGameByName(ws_preferences.cell(row=j,column=1).value))
+    for i in range(2, ws_preferences.max_column + 1):
+        for j in range(2, ws_preferences.max_row + 1):
+            Preference(ws_preferences.cell(row=j, column=i).value,
+                       GetPlayerByName(ws_preferences.cell(row=1, column=i).value),
+                       GetGameByName(ws_preferences.cell(row=j, column=1).value))
+
 
 def ImportBerger(ws_berger):
-    for i in range(1,nr_of_players):
+    for i in range(1, nr_of_players):
         round_i = Round(i)
-        for j in range(1,int(nr_of_players/2)+1):
-            pairing_nrs = ws_berger.cell(row=i,column=j).value.split(':')           
-            round_i.matches.append(Match(int(pairing_nrs[0]),int(pairing_nrs[1])))
+        for j in range(1, int(nr_of_players / 2) + 1):
+            pairing_nrs = ws_berger.cell(row=i, column=j).value.split(':')
+            round_i.matches.append(Match(int(pairing_nrs[0]), int(pairing_nrs[1])))
